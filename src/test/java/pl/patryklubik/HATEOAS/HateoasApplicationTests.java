7a6e8696e2dd860f18c1f30144ee1839e2ad9d72
Patryk Łubik <patryk.lubik@wp.pl>
2021-07-18 13:21:36 +0200

    skeleton

---
+++ src/test/java/pl/patryklubik/HATEOAS/HateoasApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.patryklubik.HATEOAS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HateoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
